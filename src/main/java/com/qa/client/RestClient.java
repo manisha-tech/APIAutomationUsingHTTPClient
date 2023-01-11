6e4ea20333f78e72f2d5392f83426ec61fb94219
manisha-tech <manishaswati.14@gmail.com>
2023-01-11 11:44:58 -0500

    GET call with headers

--- src/main/java/com/qa/client/RestClient.java
+++ src/main/java/com/qa/client/RestClient.java
@@ -2,6 +2,8 @@ package com.qa.client;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
@@ -15,30 +17,51 @@ import org.json.JSONObject;
 public class RestClient {
 
 		
-		//1. GET Method
-			public void get(String url) throws ClientProtocolException, IOException{
+		//1. GET Method without HEADERS
+	public CloseableHttpResponse get(String url) throws ClientProtocolException, IOException{
+	CloseableHttpClient httpClient = HttpClients.createDefault();
+	HttpGet httpget = new HttpGet(url); //http get request
+	CloseableHttpResponse closebaleHttpResponse =  httpClient.execute(httpget); //hit the GET URL
+	
+	return closebaleHttpResponse;
+		
+	}
+//			public void get(String url) throws ClientProtocolException, IOException{
+//			CloseableHttpClient httpClient = HttpClients.createDefault();
+//			HttpGet httpget = new HttpGet(url); //http get request
+//			CloseableHttpResponse closebaleHttpResponse =  httpClient.execute(httpget); //hit the GET URL
+
+//	    //a. Status code:
+//			int statusCode = closebaleHttpResponse.getStatusLine().getStatusCode();
+//			System.out.println("Status Code--->" + statusCode);
+//			
+//		//b. Json string:
+//			String responseString = EntityUtils.toString((HttpEntity) closebaleHttpResponse.getEntity(),"UTF-8");
+//			JSONObject responseJson=new JSONObject(responseString);
+//			System.out.println("Response JSON from API--->" + responseJson);
+//			
+//		//c. All headers
+//			Header [] headersArray= closebaleHttpResponse.getAllHeaders();
+//			HashMap<String,String>  allHeaders= new HashMap<String,String>();
+//			for(Header header:headersArray) {
+//				allHeaders.put(header.getName(),header.getValue());
+//			
+//			}
+//			System.out.println("Headers Array--->" + allHeaders);
+//          }
+
+			
+			//2. GET Method with Headers:
+			public CloseableHttpResponse get(String url, HashMap<String, String> headerMap) throws ClientProtocolException, IOException{
 			CloseableHttpClient httpClient = HttpClients.createDefault();
 			HttpGet httpget = new HttpGet(url); //http get request
-			CloseableHttpResponse closebaleHttpResponse =  httpClient.execute(httpget); //hit the GET URL
-
-	    //a. Status code:
-			int statusCode = closebaleHttpResponse.getStatusLine().getStatusCode();
-			System.out.println("Status Code--->" + statusCode);
 			
-		//b. Json string:
-			String responseString = EntityUtils.toString((HttpEntity) closebaleHttpResponse.getEntity(),"UTF-8");
-			JSONObject responseJson=new JSONObject(responseString);
-			System.out.println("Response JSON from API--->" + responseJson);
-			
-		//c. All headers
-			Header [] headersArray= closebaleHttpResponse.getAllHeaders();
-			HashMap<String,String>  allHeaders= new HashMap<String,String>();
-			for(Header header:headersArray) {
-				allHeaders.put(header.getName(),header.getValue());
-			
+			for(Map.Entry<String,String> entry : headerMap.entrySet()){
+				httpget.addHeader(entry.getKey(), entry.getValue());
 			}
-			System.out.println("Headers Array--->" + allHeaders);
-
+			CloseableHttpResponse closebaleHttpResponse =  httpClient.execute(httpget); //hit the GET URL
+			return closebaleHttpResponse;
+				
 			}
 
 }
